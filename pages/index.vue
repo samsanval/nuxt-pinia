06b6779ee426e60c373526e2d73ab66318e76044
samsanval <samsanchezalvarez@gmail.com>
2022-09-10 13:21:03 +0200

    Revert "resolve conflict"
    This reverts commit dbc94c9d94b6f6e940c1ee34b55fc6dbfcd0c8ae.

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hello</h1>
+  {{store.count}}
+  <ul>
+    <li v-for="book in books">
+      <NuxtLink :to="bookLink(book)">
+        {{book.title}}
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import {useStore} from "~/store/pinia";
+import Book from "../domain/Book";
+
+
+const { data: books } = await useFetch('http://localhost:8080/books');
+const store = useStore();
+
+const bookLink = (book: Book) => {
+    return `/book/${book.id}`
+};
+</script>
